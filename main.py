fe38a156435aa48e2a04b8e0e8a3ec89a47133e0
Elringus <elringus@gmail.com>
2014-06-25 18:40:48 +0400

    implemented getObstacles

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import webapp2
 import json
 from random import randint
-import time
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -9,13 +8,22 @@ class MainHandler(webapp2.RequestHandler):
 
 class Player(webapp2.RequestHandler):
     def get(self):
-        data = json.dumps({"PlayerPosition": [randint(-24, 24), randint(-24, 24)]})
+        data = json.dumps({'PlayerPosition': [randint(-24, 24), randint(-24, 24)]})
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(data)
 
 class Pointer(webapp2.RequestHandler):
     def get(self):
-        data = json.dumps({"PointerPosition": [randint(-24, 24), randint(-24, 24)]})
+        data = json.dumps({'PointerPosition': [randint(-24, 24), randint(-24, 24)]})
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(data)
+
+class Obstacles(webapp2.RequestHandler):
+    def get(self):
+        c = {'Obstacles': []}
+        for i in range(0, 100):
+            c['Obstacles'].append(['house' if randint(0, 3) == 3 else 'tree', randint(-24, 24), randint(-24, 24)])
+        data = json.dumps(c)
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(data)
 
@@ -23,4 +31,5 @@ app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/getPointer', Pointer),
     ('/getPlayer', Player),
+    ('/getObstacles', Obstacles),
 ], debug=True)

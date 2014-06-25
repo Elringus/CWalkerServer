b567ebe191b3769f0ae256e8cc31b370bd444ce0
Elringus <elringus@gmail.com>
2014-06-25 19:28:53 +0400

    excluded lake from the random spawn area

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ class MainHandler(webapp2.RequestHandler):
 
 class Player(webapp2.RequestHandler):
     def get(self):
-        data = json.dumps({'PlayerPosition': [randint(-24, 24), randint(-24, 24)]})
+        data = json.dumps({'PlayerPosition': get_spawn_point()})
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(data)
 
 class Pointer(webapp2.RequestHandler):
     def get(self):
-        data = json.dumps({'PointerPosition': [randint(-24, 24), randint(-24, 24)]})
+        data = json.dumps({'PointerPosition': get_spawn_point()})
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(data)
 
@@ -22,10 +22,20 @@ class Obstacles(webapp2.RequestHandler):
     def get(self):
         c = {'Obstacles': []}
         for i in range(0, 100):
-            c['Obstacles'].append(['house' if randint(0, 3) == 3 else 'tree', randint(-24, 24), randint(-24, 24)])
+            spawn_point = get_spawn_point()
+            c['Obstacles'].append(['house' if randint(0, 3) == 3 else 'tree', spawn_point[0], spawn_point[1]])
         data = json.dumps(c)
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(data)
+
+# exclude lake from the spawn area
+def get_spawn_point():
+    quadrant = randint(1, 3)
+    return {
+        1: [randint(8, 24), randint(-24, 24)],
+        2: [randint(-10, 9), randint(-24, -4)] if randint(0, 1) == 0 else [randint(-11, 9), randint(11, 24)],
+        3: [randint(-24, -11), randint(-24, 24)],
+    }[quadrant]
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
